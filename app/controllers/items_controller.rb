f78a40712da955415a9a53f4d40e19d4828ada53
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-03 23:19:37 -0400

    commit tree 5f2bb30f55e0af9e2ad0e094961aced5b818a117

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+      return render json: items, include: :user
+    else
+      items = Item.all
+      return render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find_by!(id: params[:user_id])
+    item =  user.items.create(name: params[:name], description: params[:description], price: params[:price])
+    render json: item, status: :created
+  end
+
+  private
+
+  def user_not_found
+    render json: {error: "User not found"}, status: :not_found
   end
 
 end
